c62ab2f0bd715ca7af62d4e8b29296cbf91053d5
Toby <Toby@Laptop-Windows10.com>
2024-03-13 09:22:49 +0800

    优化登录流程

--- 项目源码/student-mangement-system-web/src/api/request.ts
+++ 项目源码/student-mangement-system-web/src/api/request.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { useUserStore } from '../store/modules/user'
-const token = useUserStore().token
 
 const service  = axios.create({
     // baseURL: import.meta.env.VITE_APP_BASE_API,
@@ -12,7 +11,8 @@ const service  = axios.create({
 // Axios 请求前置拦截器 | 加入token
 service.interceptors.request.use( config => {
     if (!config.headers) config.headers = {};
-    token? config.headers.token= token : ''
+    let token = useUserStore().token
+    token? config.headers.token = token : ''
     return config
 }, error => {
     console.log("[Interceptors Error]" + error);
